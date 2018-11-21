0168790ce35bdecd0514e1b42b566140695fd548
mkslofstra <mariska-karina@hotmail.com>
2018-11-21 16:33:26 +0100

    WIP: facet option

---
+++ src/components/FacetOption.vue
@@ -0,0 +1,29 @@
+<template>
+    <b-button :variant="variant" :active="active">{{label}}</b-button>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'FacetOption',
+        props: {
+            label: {
+                type: String,
+                required: true,
+            },
+            active: {
+                type: Boolean,
+                required: false,
+                default: false,
+            },
+        },
+        computed: {
+            variant () {
+                return this.active ? "primary" : "outline-primary"
+            }
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/LifelinesWebshop.vue
@@ -0,0 +1,22 @@
+<template>
+    <b-container fluid>
+        <b-row>
+            <b-column>
+                <FacetOption label=test :active=false></FacetOption>
+            </b-column>
+        </b-row>
+    </b-container>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import FacetOption from '../components/FacetOption.vue';
+
+    export default Vue.extend({
+        name: 'LifelinesWebshop',
+        components: {FacetOption},
+        props: {
+            msg: String,
+        },
+    });
+</script>
