518149a3144d196ee606cb4c0d035ffbe2bbc863
mkslofstra <mariska-karina@hotmail.com>
2018-11-22 14:18:40 +0100

    [FEATURE] Facet component

---
+++ src/components/Facet.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <b-row>
+            <b-col>
+                <h4>{{this.title}}: <small v-if="this.description"><font-awesome-icon icon="question-circle" /></small></h4>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col>
+                <FacetOption v-for="option in this.options" :label="option.label"></FacetOption>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+  import FacetOption from '../components/FacetOption.vue';
+  export default {
+    name: 'Facet',
+    components: {FacetOption},
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: false,
+      },
+      options: {
+        type: Array,
+        required: true,
+      },
+    },
+  };
+</script>
+
+<style scoped>
+</style>
--- src/views/LifelinesWebshop.vue
+++ src/views/LifelinesWebshop.vue
@@ -1,8 +1,30 @@
 <template>
     <b-container fluid>
         <b-row>
+            <b-col md="8">
+                <h1>Lifelines data item selection</h1>
+            </b-col>
+            <b-col md="4">
+                <div>
+                    <b-button>Save</b-button>
+                    <b-button>Reset</b-button>
+                    <b-button>Request</b-button>
+                </div>
+            </b-col>
+        </b-row>
+        <b-row>
             <b-col>
-                <FacetOption label="facet option" :active=false></FacetOption>
+                <Facet :title="this.$store.state.categorical_facets.ageGroup.label" :description="this.$store.state.categorical_facets.ageGroup.description" :options="this.$store.state.categorical_facets.ageGroup.options"></Facet>
+                <Facet :title="this.$store.state.categorical_facets.sexGroup.label" :description="this.$store.state.categorical_facets.sexGroup.description" :options="this.$store.state.categorical_facets.sexGroup.options"></Facet>
+                <Facet :title="this.$store.state.categorical_facets.subCohorts.label" :description="this.$store.state.categorical_facets.subCohorts.description" :options="this.$store.state.categorical_facets.subCohorts.options"></Facet>
+                <Facet :title="this.$store.state.categorical_facets.collectionPoint.label" :description="this.$store.state.categorical_facets.collectionPoint.description" :options="this.$store.state.categorical_facets.collectionPoint.options"></Facet>
+
+            </b-col>
+            <b-col>
+               Topic Tree
+            </b-col>
+            <b-col>
+                Data items
             </b-col>
         </b-row>
     </b-container>
@@ -10,11 +32,11 @@
 
 <script>
     import Vue from 'vue';
-    import FacetOption from '../components/FacetOption.vue';
+    import Facet from '../components/Facet.vue';
 
     export default Vue.extend({
         name: 'LifelinesWebshop',
-        components: {FacetOption},
+        components: {Facet},
         props: {
             msg: String,
         },
