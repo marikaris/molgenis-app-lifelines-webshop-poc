11c02190ab41906dcd24fbc9e0aa065020e91186
connor <c.stroomberg@gmail.com>
2018-11-22 22:03:12 +0100

    move backend call to repositories
    todo: use repo prototype or resource , but hey it's a proto type

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,88 +1,42 @@
-// @ts-ignore
-import api from '@molgenis/molgenis-api-client'
-import Topic from '@/types/store/topic'
-import DataItem from '@/types/store/dataItem'
-import CategoricalFacet from '@/types/store/categoricalFacet'
-import CategoricalFacetOption from '@/types/store/categoricalFacetOption'
-
-const refToId = (ref: any): string => ref.id
-
-const toDataItem = (item: any): DataItem => {
-  return {
-    id: item.id,
-    ageGroups: item.ageGroups,
-    sexGroups: item.sexGroups.map(refToId),
-    subCohorts: item.subcohorts.map(refToId),
-    collectionPoints: item.collections.map(refToId),
-    topic: item.topic.id,
-    label: item.label,
-    ordinalPosition: item.ordinalPosition,
-    description: item.description
-  }
-}
-
-const toTopic = (item: any): Topic => {
-  const topic: Topic = {
-    id: item.id,
-    label: item.label
-  }
-
-  if (item.parent) {
-    topic.parentTopicId = refToId(item.parent)
-  }
-
-  return topic
-}
-
-const toCategoricalFacetOption = (item: any): CategoricalFacetOption => {
-  return {
-    id: item.id,
-    label: item.label
-  }
-}
-
-const toCategoricalFacet = (response: any): CategoricalFacet => {
-  return {
-    label: response.meta.label,
-    options: response.items.map(toCategoricalFacetOption)
-  }
-}
+import dataItemRepository from '@/repository/dataItemRepository'
+import topicRepository from '@/repository/topicRepository'
+import categoricalFacetRepository from '@/repository/categoricalFacetRepository'
 
 export default {
 
-  getDataItems ({ commit }: any) {
-    api.get('api/v2/lifelines_dataItems').then((response: any) => {
-      commit('setDataItems', response.items.map(toDataItem))
-    })
+  getDataItems ({ commit }: any): void {
+    dataItemRepository.getAll().then((dataItems) => {
+      commit('setDataItems', dataItems)
+    }).catch()
   },
 
   getTopics ({ commit }: any) {
-    api.get('api/v2/lifelines_topics').then((response: any) => {
-      commit('setTopics', response.items.map(toTopic))
-    })
+    topicRepository.getAll().then((topics) => {
+      commit('setTopics', topics)
+    }).catch()
   },
 
   getAgeGroups ({ commit }: any) {
-    api.get('api/v2/lifelines_ageGroups').then((response: any) => {
-      commit('setAgeGroups', toCategoricalFacet(response))
-    })
+    categoricalFacetRepository.getAgeGroups().then((ageGroups) => {
+      commit('setAgeGroups', ageGroups)
+    }).catch()
   },
 
   getSexGroups ({ commit }: any) {
-    api.get('api/v2/lifelines_sexGroups').then((response: any) => {
-      commit('setSexGroups', toCategoricalFacet(response))
-    })
+    categoricalFacetRepository.getSexGroups().then((sexGroups) => {
+      commit('setSexGroups', sexGroups)
+    }).catch()
   },
 
   getSubCohorts ({ commit }: any) {
-    api.get('api/v2/lifelines_subcohorts').then((response: any) => {
-      commit('setSubCohorts', toCategoricalFacet(response))
-    })
+    categoricalFacetRepository.getSubCohorts().then((subCohorts) => {
+      commit('setSubCohorts', subCohorts)
+    }).catch()
   },
 
   getCollectionPoints ({ commit }: any) {
-    api.get('api/v2/lifelines_collections').then((response: any) => {
-      commit('setCollectionPoints', toCategoricalFacet(response))
-    })
+    categoricalFacetRepository.getCollectionPoints().then((collectionPoints) => {
+      commit('setCollectionPoints', collectionPoints)
+    }).catch()
   }
 }
