9abb88ff0927cd5b791582b3f386626c3bbef5e5
mkslofstra <mariska-karina@hotmail.com>
2018-11-23 15:14:58 +0100

    Add some styling

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,10 @@ import '@/assets/lifelines_bs4.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faQuestionCircle, faSave, faUndo } from '@fortawesome/free-solid-svg-icons'
+import { faQuestionCircle, faSave, faUndo, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faQuestionCircle, faSave, faUndo)
+library.add(faQuestionCircle, faSave, faUndo, faSearch)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
--- src/views/LifelinesWebshop.vue
+++ src/views/LifelinesWebshop.vue
@@ -1,7 +1,44 @@
 <template>
-    <b-container fluid>
-
+    <b-container fluid class="mt-2">
         <b-row>
+            <b-col class="mb-2">
+                <div class="float-right">
+                    <b-button variant="outline-secondary" class="mr-1 my-2 my-sm-0" type="submit">
+                        <font-awesome-icon icon="save"/>
+                        Save
+                    </b-button>
+                    <b-button variant="outline-secondary" class="mr-1 my-2 my-sm-0" type="submit">
+                        <font-awesome-icon icon="undo"/>
+                        Reset
+                    </b-button>
+                    <b-button variant="outline-info" class="mr-1 my-2 my-sm-0" type="submit">Request</b-button>
+                </div>
+
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col md="3">
+                <h3 class="nav-title font-weight-bold">Filter items</h3>
+            </b-col>
+            <b-col md="6">
+                <div>
+                    <b-input-group>
+                        <b-form-input
+                                class="search-bar"
+                                type="text"
+                                placeholder="Search">
+                        </b-form-input>
+
+                        <b-input-group-append>
+                            <b-btn variant="outline-secondary" class="search-icon">
+                                <font-awesome-icon icon="search"/>
+                            </b-btn>
+                        </b-input-group-append>
+                    </b-input-group>
+                </div>
+            </b-col>
+        </b-row>
+        <b-row class="mt-2">
             <b-col md="3">
                 <Facet :categoricalFacet="this.$store.state.categoricalFacets.ageGroup"></Facet>
                 <Facet :categoricalFacet="this.$store.state.categoricalFacets.sexGroup"></Facet>
@@ -9,7 +46,7 @@
                 <Facet :categoricalFacet="this.$store.state.categoricalFacets.collectionPoint"></Facet>
             </b-col>
             <b-col md="2">
-               Topic Tree
+                Topic Tree
             </b-col>
             <b-col md="7">
                 <data-items :dataItems="vueDataItems"></data-items>
@@ -26,7 +63,7 @@
 
   export default Vue.extend({
     name: 'LifelinesWebshop',
-    components: { Facet, DataItems },
+    components: {Facet, DataItems},
     props: {
       msg: String
     },
@@ -53,3 +90,19 @@
     }
   })
 </script>
+<style scoped>
+    .search-icon {
+        color: #495057;
+        border-color: #ced4da;
+        border-style: solid;
+        border-width: 1px 1px 1px 0px;
+    }
+    .search-bar {
+        border-width: 1px 0px 1px 1px;
+    }
+
+    h3.nav-title{
+        color: #276daa;
+    }
+</style>
+
