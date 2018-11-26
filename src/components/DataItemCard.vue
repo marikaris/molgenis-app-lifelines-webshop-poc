4fd525d0ea14df783ca0dc500d5f3a613aa96570
connor <c.stroomberg@gmail.com>
2018-11-26 13:19:44 +0100

    Make data item badges disabled when data item is disabled

--- src/components/DataItemCard.vue
+++ src/components/DataItemCard.vue
@@ -47,4 +47,14 @@
    button.list-group-item[disabled] {
     background-color: #cccccc24;
   }
+
+   button.list-group-item:disabled span,
+   button.list-group-item[disabled] span {
+     color: #6c757d;
+   }
+
+   button.list-group-item:disabled svg,
+   button.list-group-item[disabled] svg {
+     color: #6c757d;
+   }
 </style>
