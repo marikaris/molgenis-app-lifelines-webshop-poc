ab278bd4f846293cd5b327193cbaa74e1936133f
mkslofstra <mariska-karina@hotmail.com>
2018-11-27 15:11:24 +0100

    Add carets

--- src/components/TopicTree.vue
+++ src/components/TopicTree.vue
@@ -1,32 +1,48 @@
 <template>
-  <b-list-group>
-    <b-list-group-item v-for="topic in topicList"
-                       :key="topic.id"
-                       :button="true"
-                       :variant="topic.children.length ? 'secondary' : 'default'"
-                       :active="topic.selected"
-                       @click="topicClick(topic)">
-      {{topic.label}}
-    </b-list-group-item>
-  </b-list-group>
+    <b-list-group>
+        <b-list-group-item v-for="topic in topicList"
+                           :key="topic.id"
+                           :button="true"
+                           :variant="topic.children.length ? 'secondary' : 'default'"
+                           :active="topic.selected"
+                           @click="topicClick(topic)">
+      <span v-if="topic.children.length">
+        <font-awesome-icon class="caret-icon" icon="caret-down" v-if="isOpenTopic(topic)"></font-awesome-icon>
+          <font-awesome-icon class="caret-icon" icon="caret-right" v-else></font-awesome-icon>  {{topic.label}}
+      </span>
+            <span v-else class="p-1">
+        {{topic.label}}
+      </span>
+
+        </b-list-group-item>
+    </b-list-group>
 </template>
 <script>
-import { mapGetters, mapMutations } from 'vuex'
+  import { mapGetters, mapMutations } from 'vuex'
 
-export default {
-  name: 'Topic',
-  methods: {
-    topicClick (topic) {
-      if (topic.children.length) {
-        this.toggleTopicOpen(topic.id)
-      } else {
-        this.toggleTopicSelect(topic.id)
-      }
+  export default {
+    name: 'Topic',
+    methods: {
+      topicClick (topic) {
+        if (topic.children.length) {
+          this.toggleTopicOpen(topic.id)
+        } else {
+          this.toggleTopicSelect(topic.id)
+        }
+      },
+      isOpenTopic (topic) {
+        return this.$store.state.openTopics.includes(topic.id)
+      },
+      ...mapMutations(['toggleTopicSelect', 'toggleTopicOpen'])
     },
-    ...mapMutations(['toggleTopicSelect', 'toggleTopicOpen'])
-  },
-  computed: {
-    ...mapGetters(['topicList'])
+    computed: {
+      ...mapGetters(['topicList'])
+    }
   }
-}
 </script>
+<style scoped>
+    .caret-icon {
+        margin-left: -1rem;
+        width: 1rem;
+    }
+</style>
